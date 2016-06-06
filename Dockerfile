0e8bf6aaf002dd0147eeca9c6026a0a666d3babf
lizw <zwli@dataman-inc.com>
2016-06-06 09:45:16 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM index.shurenyun.com/zhguo/alpine:3.3
+MAINTAINER will <g.success16@gmail.com> 
+
+ADD hello /hello
+CMD ./hello
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 1000000; i++ {
+		fmt.Println("keep calm and carry on")
+		fmt.Println("no zuo no die")
+		time.Sleep(time.Second * 10)
+	}
